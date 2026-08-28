745462138c486b43d99a334a31ce73f07e5aeed8
Ankit Kushwah <186614665+Mrankit47@users.noreply.github.com>
2026-08-28 13:11:21 +0530

    ✅ Solve: Average Time of Process per Machine (MySQL)

--- Easy/average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ Easy/average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,5 +1,11 @@
-SELECT w2.id
-FROM Weather w1
-JOIN Weather w2
-    ON DATEDIFF(w2.recordDate, w1.recordDate) = 1
-WHERE w2.temperature > w1.temperature;
+# Write your MySQL query statement below
+SELECT
+    a.machine_id,
+    ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+FROM Activity a
+JOIN Activity b
+    ON a.machine_id = b.machine_id
+    AND a.process_id = b.process_id
+    AND a.activity_type = 'start'
+    AND b.activity_type = 'end'
+GROUP BY a.machine_id;
